7fe446cff7c1aaadfcafd259fddb0d13aff33255
Warintara Munsup <118775607+rinrinthefrog@users.noreply.github.com>
2023-01-20 23:50:56 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -15,8 +15,26 @@ robot = Robot(r_gauche,r_droite)
 # affichage du robot
 print(robot) # affichage --> Le robot en position (0,0) est à l'arret
 
+from Robot import * # Permet d'utiliser la classe Robot se trouvant dans le meme repertoire
+from Roue import * # Permet d'utiliser la classe Roue se trouvant dans le meme repertoire
+
+# instanciation de deux roues
+r_gauche = Roue(3,10)
+r_droite = Roue(3,10)
+
+# affichage de chaque roue
+print(r_gauche) # affichage --> Roue de taille 3 cm est à l'arret
+print(r_droite) # affichage --> Roue de taille 3 cm est à l'arret
+
+
+# instanciation d'un robot, prenant en parametre les deux roue créer précédemment
+robot = Robot(r_gauche,r_droite)
+
+# affichage du robot
+print(robot) # affichage --> Le robot en position (0,0) est à l'arret
+
 
 # modification la vitesse des roues d'un robot 
-robot.roue_gauche.setVitesse(10)
-robot.roue_droite.setVitesse(10)
-print(robot)
+robot.roue_gauche.setVitesse(5)
+robot.roue_droite.setVitesse(5)
+

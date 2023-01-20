ab887a25116f3580714018f06c6763bacccd51aa
zelievdm <122898750+zelievdm@users.noreply.github.com>
2023-01-20 17:33:14 +0100

    Update Roue.py

--- Roue.py
+++ Roue.py
@@ -1,3 +1,5 @@
+from math import pi
+
 class Roue :
 	def __init__ (self, taille_cm, vMaxTourParSec) :
 		self.taille_cm = taille_cm
@@ -17,3 +19,17 @@ class Roue :
 		else : # Si la roue tourne
 			res += " roule à " + str(self.vTourParSec) + "tour/seconde"
 		return res
+	
+	def setVitesse(self,vitesseVoulue_kmh):
+		"""
+		Formule de conversion de la vitesse v en km/h en vitesse de rotation n en tr/s :
+		N=(3600*v)/(2*pi*rayon)
+		"""
+		vVoulueTourParSec=(3600*vitesseVoulue_kmh)/(2*math.pi*this.taille_cm)
+		if (vVoulueTourParSec>vMaxTourParSec): # Si la vitesse voulue est plus grande que la vitesse maximale possible
+			self.vTourParSec=vMaxTourParSec
+			print("La vitesse voulue est trop grande, les roues tournent à leur vitesse maximale")
+		else: # Si la vitesse est possible pour la roue
+			self.vTourParSec=vVoulueTourParSec
+		return self.vTourParSec
+		

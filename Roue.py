0a770cfcf031607149c023a53eb0561424729653
Warintara Munsup <118775607+rinrinthefrog@users.noreply.github.com>
2023-01-21 15:07:52 +0100

    Update Roue.py

--- Roue.py
+++ Roue.py
@@ -1,19 +1,38 @@
+import math
+
 class Roue :
-	def __init__ (self, taille_cm, vMaxTourParSec) :
-		self.taille_cm = taille_cm
-		self.vMaxTourParSec = vMaxTourParSec
-		self.vTourParSec = 0
-
-	def __str__ (self) :
+    def __init__ (self, taille_cm, vMaxTourParSec) :
+        self.taille_cm = taille_cm
+        self.vMaxTourParSec = vMaxTourParSec
+        self.vTourParSec = 0
+    
+    def setVitesse(self,vitesseVoulue_kmh):
+        """
+		Formule de conversion de la vitesse v en km/h en vitesse de rotation n en tr/s :
+		N=(25*v)/(3*60*pi*rayon_en_metre)
 		"""
-		Equivalent methode toString(Java)
-		Permet de redéfinir la methode print(monInstance)
-		""" 
-		res = "Roue de taille " + str(self.taille_cm) + " cm"
-		
-		if self.vTourParSec == 0: # Si la roue est à l'arret
-			res += " est à l'arret" 
+        vVoulueTourParSec= (25*vitesseVoulue_kmh/(3*math.pi*self.taille_cm*0.01))/60
 
-		else : # Si la roue tourne
-			res += " roule à " + str(self.vTourParSec) + "tour/seconde"
-		return res
+        if (vVoulueTourParSec>self.vMaxTourParSec):# Si la vitesse voulue est plus grande que la vitesse maximale possible 
+            self.vTourParSec=self.vMaxTourParSec
+            print("La vitesse voulue est trop grande, les roues tournent à leur vitesse maximale")
+            
+        else: # Si la vitesse est possible pour la roue
+            self.vTourParSec=vVoulueTourParSec
+        
+        return self.vTourParSec
+
+
+    
+    def __str__ (self) :
+        """
+    		Equivalent methode toString(Java)
+    		Permet de redéfinir la methode print(monInstance)
+        """ 
+        res = "Roue de taille " + str(self.taille_cm) + " cm"
+        if self.vTourParSec == 0: # Si la roue est à l'arret
+            res += " est à l'arret" 
+
+        else : # Si la roue tourne
+            res += " roule à " + str(self.vTourParSec) + "tour/seconde"
+        return res

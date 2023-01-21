5955280808a96175c56e12cf6d1fb065f0bcdae7
Warintara Munsup <118775607+rinrinthefrog@users.noreply.github.com>
2023-01-21 16:41:03 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 from Robot import * # Permet d'utiliser la classe Robot se trouvant dans le meme repertoire
 from Roue import * # Permet d'utiliser la classe Roue se trouvant dans le meme repertoire
 
+
 # instanciation de deux roues
 r_gauche = Roue(3,10)
 r_droite = Roue(3,10)
@@ -13,13 +14,13 @@ print(r_droite) # affichage --> Roue de taille 3 cm est à l'arret
 # instanciation d'un robot, prenant en parametre les deux roue créer précédemment
 robot = Robot(r_gauche,r_droite)
 
+
 # affichage du robot
 print(robot) # affichage --> Le robot en position (0,0) est à l'arret
 
-# avancer le robot par le modification la vitesse des roues d'un robot 
-robot.avancer(1)
-
-robot.avancer(1)
-
-robot.reculer(2)
+robot.avancer(5,10) # avancer le robot à la vitesse de 5 km/h en 10 seconde
+print(robot)
+robot.reculer(3,10) # reculer le robot à la vitesse de 5 km/h en 10 seconde
+print(robot)
+robot.arreter_urgence() # arrêter le robot d'urgence 
 print(robot)
